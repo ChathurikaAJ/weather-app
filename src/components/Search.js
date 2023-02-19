835a0251498d06781aef7ed05f3e3f99d952cc8e
Chathurika <chathurika-jayakody.com>
2023-02-19 14:28:34 -0500

    Add weather and update search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,21 @@
 import { BsSearch } from 'react-icons/bs'
 
 const Search = ({getWeather}) => {
+
     return (
-        <div className='flex justify-between items-center max-w-[500px] w-full m-auto pt-4 px-4 bg-red z-50'>
-            <form >
+        <div className=' relative flex justify-between items-center max-w-[500px] w-full m-auto pt-4 px-4 text-white z-10'>
+            <form 
+                onSubmit={getWeather}
+                className='flex justify-between items-center w-full m-auto p-3 bg-white  text-white rounded-2xl'>
                 <div>
-                    <input type="text" placeholder="Search city" />
+                    <input 
+                        type="text" 
+                        placeholder="Search city" 
+                        className='bg-transparent border-none text-black focus:outline-none text-2xl'
+                        name="cityName"
+                    />
                 </div>
-                <button onClick={getWeather}><BsSearch /></button>
+                <button type='submit'><BsSearch color='gray' /></button>
             </form>
         </div>
     )
---
+++ src/components/Weather.js
@@ -0,0 +1,10 @@
+
+
+const Weather = ({weatherData}) => {
+    console.log(weatherData)
+  return (
+    <div>Weather</div>
+  )
+}
+
+export default Weather
